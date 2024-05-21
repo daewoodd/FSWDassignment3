ee884f8a62f5330b5cd7450516af5c2da17e70d5
daewoodd <qrow2112@gmail.com>
2024-05-21 19:39:07 +0500

    Minor NavBar modifications

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,50 +1,26 @@
-// src/components/Header.jsx
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const NavBar = () => (
-  <Navbar
-    style={{
-      backgroundColor: "#3498db",
-      height: "80px",
-      borderBottom: "2px solid #2980b9",
-    }}
-    variant="dark"
-    expand="lg"
-  >
-    <Navbar.Brand
-      as={Link}
-      to="/"
-      style={{ color: "#fff", fontSize: "24px", fontWeight: "bold" }}
-    >
+  <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar.Brand as={Link} to="/">
       Portfolio
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/" style={{ color: "#fff", fontSize: "18px" }}>
+        <Nav.Link as={Link} to="/" style={{ color: "#fff" }}>
           Home
         </Nav.Link>
-        <Nav.Link
-          as={Link}
-          to="/skills"
-          style={{ color: "#fff", fontSize: "18px" }}
-        >
+        <Nav.Link as={Link} to="/skills" style={{ color: "#fff" }}>
           Skills
         </Nav.Link>
-        <Nav.Link
-          as={Link}
-          to="/projects"
-          style={{ color: "#fff", fontSize: "18px" }}
-        >
+        <Nav.Link as={Link} to="/projects" style={{ color: "#fff" }}>
           Projects
         </Nav.Link>
-        <Nav.Link
-          as={Link}
-          to="/contact"
-          style={{ color: "#fff", fontSize: "18px" }}
-        >
+        <Nav.Link as={Link} to="/contact" style={{ color: "#fff" }}>
           Contact
         </Nav.Link>
       </Nav>
